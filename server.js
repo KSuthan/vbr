361d0e78b1fcd5b03d21d5b9198a0c98435aca2c
Suthan kathir <suthancode2gmail.com>
2022-01-12 21:50:29 -0500

    add pagesize command

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const inquirer = require("inquirer");
-// const { connection } = require("./db");
+const { connection } = require("./db");
 const db = require("./db")
 
 const connections = require("./db/connection");
@@ -21,7 +21,7 @@ function mainMenu() {
             type: "list",
             name: "choice",
             message: "What would you like to do?",
-            choices: [ "View Employees", "view by role", "budget by department" ]
+            choices: [ "View Employees", "add role", "view by role", "budget by department" ]
         },
     ])
 
@@ -36,6 +36,10 @@ function mainMenu() {
                 case "budget by department":
                     totalbudget();
                     break;
+
+                    case "add role":
+                        addRole();
+                         break;
 
                 case "view by role":
                     viewByrole()
@@ -205,31 +209,64 @@ function mainMenu() {
 // }
 
 
+// function   viewByrole() {
+//     var sql = "SELECT first_name FROM roles";
+//     let newArr = []
+//     connections.query(sql, function(err, results){
+//         if (err){
+//           throw err;
+//         }
+//         newArr = results.map(data=>data.title)
+//     }).then(function(roles){
+//         for (i=0; i < roles.length; i++){
+//            newArr.push(roles[i].title);
+//         }
+
+//         inquirer.prompt({
+//                       name: "roles",
+//                       type: "list",
+//                       message: "Which role would you like to search?",
+//                       choices: newArr,
+//                       pageSize: 3
+      
+//                  }).then((answer) => {
+      
+//                                   const query = `SELECT e.id AS ID, e.first_name AS firstname, e.last_name AS LastName, roles.title AS Title, department.name AS Department, roles.salary AS Salary, concat(m.first_name, ' ' ,  m.last_name) AS Manager 
+//                                   FROM employee e LEFT JOIN employee m ON e.manager_id = m.id 
+//                                   INNER JOIN roles ON e.role_id = roles.id 
+//                                   INNER JOIN department ON roles.department_id = department.id WHERE roles.title = '${answer.roles}';`
+//                                   connections.query(query, (err, res) => {
+//                                     if(err) return err;
+                                  
+//                           console.table(res);
+//                           mainMenu();
+//                  })
+//                 })
+//             })                 
+//     }
+
+
 function   viewByrole() {
-    var sql = "SELECT first_name FROM roles";
+    var sql = "SELECT title FROM roles";
     let newArr = []
     connections.query(sql, function(err, results){
         if (err){
           throw err;
         }
         newArr = results.map(data=>data.title)
-    }).then(function(roles){
-        for (i=0; i < roles.length; i++){
-           newArr.push(roles[i].title);
-        }
-
         inquirer.prompt({
-                      name: "roles",
+                      name: "role",
                       type: "list",
                       message: "Which role would you like to search?",
-                      choices: newArr
+                      choices: newArr,
+                      pageSize: 15
       
                  }).then((answer) => {
       
                                   const query = `SELECT e.id AS ID, e.first_name AS firstname, e.last_name AS LastName, roles.title AS Title, department.name AS Department, roles.salary AS Salary, concat(m.first_name, ' ' ,  m.last_name) AS Manager 
                                   FROM employee e LEFT JOIN employee m ON e.manager_id = m.id 
                                   INNER JOIN roles ON e.role_id = roles.id 
-                                  INNER JOIN department ON roles.department_id = department.id WHERE roles.title = '${answer.roles}';`
+                                  INNER JOIN department ON roles.department_id = department.id WHERE roles.title = '${answer.role}';`
                                   connections.query(query, (err, res) => {
                                     if(err) return err;
                                   
@@ -239,3 +276,31 @@ function   viewByrole() {
                 })
             })                 
     }
+
+    function addRole() {
+        inquirer
+        .prompt([
+            {
+              type: "input",
+              message: "Enter the Title",
+              name: "title"
+            },
+            {
+              type: "input",
+              message: "Enter the employee's salary",
+              name: "salary"
+            },
+            {
+              type: "input",
+              message: "department ID",
+              name: "deptid"
+            }
+          ])
+          .then ((answer) => {
+              const newRole = `INSERT INTO roles (title, salary, department_id) VALUES ("${answer.title}" , ${answer.salary}, ${answer.deptid})`
+              connection.promise().query (newRole)
+              console.log(`\n Title ${answer.title} ADDED \n`)
+              mainMenu();
+            })
+        
+        }
